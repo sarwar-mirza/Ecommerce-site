10dffda264767f3adfbf9e254778bc9e7ae97b7e
mithu <nilchirkutpoison@gmail.com>
2023-10-23 22:58:54 +0600

    Create Superuser Register Model Admin and show this table

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (Customer, Product, Cart, OderPlaced)
 
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'name', 'aria', 'city', 'division', 'zipcode']
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'selling_price', 'discount_price', 'brand', 'category', 'product_image']
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'quantity']
+
+
+@admin.register(OderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']
