96477e5d0e78fb46d8d754a09961243505b05def
mithu <nilchirkutpoison@gmail.com>
2023-10-24 11:59:56 +0600

    Product showing home page(css, Javascript) added

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Customer, Product, Cart, OderPlaced
 
-# Create your views here.
+
+
+# HOME CLASS VIEW INHERITE VIEW FILE
 class ProductHomeView(View):
     def get(self, request):
-        return render(request, 'app/home.html')
+        baseball = Product.objects.filter(category='B')
+        football = Product.objects.filter(category='F')
+        cricket = Product.objects.filter(category='C')
+
+        context = {'baseball': baseball, 'football': football, 'cricket': cricket}
+        return render(request, 'app/home.html', context)
+    
+
+
